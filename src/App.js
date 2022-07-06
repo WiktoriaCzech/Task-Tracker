e2804ae0bb29a1b56810b7563926804c5ffa8b3f
Wiktoria Czech <czech.wiktoria1@gmail.com>
2022-07-06 19:05:40 +0200

    wprowadzone poprawki

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+    const [showAddTask,setShowAddTask] = useState(false)
+   //list of tasks
+  const [tasks, setTasks] = useState([
+        {
+            id:1,
+            text: 'Doc Appointment',
+            day: '5th Feb 2022 at 2:30pm ',
+            reminder: true,
+        },
+        {
+            id:2,
+            text: 'Meeting with friends',
+            day: 'Tommorrow at 5 pm',
+            reminder: true,
+        },
+        {
+            id:3,
+            text: 'Buy groceries',
+            day: 'Today',
+            reminder: false,
+        },
+    ])
+  //Add Task
+    const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000)+1
+      const newTask = {id, ...task}
+        setTasks([...tasks,newTask])
+    }
+  //Delete Task
+  const deleteTask = (id) => {
+      setTasks(tasks.filter( (task) => task.id !== id))
+  }
+  //toggle reminder
+    const toggleReminder = (id) => {
+      setTasks(tasks.map((task) => task.id === id
+      ? {...task, reminder: !task.reminder } : task))
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header  onAdd = {() => setShowAddTask(!showAddTask)}
+      showAdd = {showAddTask}
+      />
+        <h6>to set the reminder double click on the task</h6>
+        {/*//short form of if ? : without else*/}
+        {showAddTask && <AddTask onAdd = {addTask} />}
+      {tasks.length > 0 ?
+          (<Tasks tasks = {tasks} onDelete = { deleteTask }
+            onToggle = {toggleReminder}/>)
+          : ('There are no tasks left, good job!')
+      }
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import  {faTimes } from '@fortawesome/free-solid-svg-icons'
+const Task = ({task, onDelete,onToggle}) => {
+    return (
+        <div className= {`task ${task.reminder ? 
+            'reminder' : ''}`} onDoubleClick = {() => onToggle(task.id)}>
+            <h3>{task.text} <FontAwesomeIcon icon= { faTimes }
+                style={{color: '#492f29', cursor: 'pointer'}}
+                onClick={ () => onDelete (task.id)}
+            /> </h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
+export default Task
